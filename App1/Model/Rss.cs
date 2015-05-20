526598e9aa4ee71066778bdbb973908e5c543927
RafalManka <rafal@manka.info>
2015-05-20 17:13:57 +0200

    initital commit

---
+++ App1/Model/Rss.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace App1.Model
+{
+    [XmlRoot("rss")]
+    public class Rss
+    {
+        [XmlElement("channel")]
+        public Channel Channel { get; set; }
+    }
+
+    public class Channel {
+        [XmlElement("item")]
+        public ObservableCollection<Item> Items { get; set; }
+
+        public Channel() {
+            this.Items = new ObservableCollection<Item>();
+        }
+    }
+    public class Item {
+        [XmlElement("title")]
+        public string title { get; set; }
+        [XmlElement("link")]
+        public string link { get; set; }
+        [XmlElement("description")]
+        public string description { get; set; }
+    }
+}
---
+++ App1/ViewModel/MainViewModel.cs
@@ -0,0 +1,33 @@
+﻿using App1.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Http;
+using System.Xml.Serialization;
+using System.ComponentModel;
+
+namespace App1.ViewModel
+{
+    class MainViewModel:INotifyPropertyChanged
+    {
+        public Rss Rss { get; set; }
+        public bool IsLoaded { get; set; }
+        public async void LoadRss(string url) {
+            HttpClient client = new HttpClient();
+            using (var Stream = await client.GetStreamAsync(url)) {
+                XmlSerializer Serializer = new XmlSerializer(typeof(Rss));
+                this.Rss = Serializer.Deserialize(Stream) as Rss;
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void NotifyPropertyChanged(string property) {
+            if (PropertyChanged != null) {
+                PropertyChanged(this, new PropertyChangedEventArgs(property));
+            }
+        }
+    }
+}
