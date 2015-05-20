f3f53d8ddbd58f6c27f125bcd5f9853b895779b9
RafalManka <rafal@manka.info>
2015-05-20 17:41:35 +0200

    renaming

---
+++ RssReader/ViewModel/MainViewModel.cs
@@ -0,0 +1,44 @@
+﻿using RssReader.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Http;
+using System.Xml.Serialization;
+using System.ComponentModel;
+
+namespace RssReader.ViewModel
+{
+    class MainViewModel:INotifyPropertyChanged
+    {
+        private Rss _rss;
+
+        public Rss Rss {
+            get {
+                return this._rss;
+            }
+
+            set {
+                this._rss = value;
+                NotifyPropertyChanged("Rss");
+            } 
+        }
+        public bool IsLoaded { get; set; }
+        public async void LoadRss(string url) {
+            HttpClient client = new HttpClient();
+            using (var Stream = await client.GetStreamAsync(url)) {
+                XmlSerializer Serializer = new XmlSerializer(typeof(Rss));
+                this.Rss = Serializer.Deserialize(Stream) as Rss;
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void NotifyPropertyChanged(string property) {
+            if (PropertyChanged != null) {
+                PropertyChanged(this, new PropertyChangedEventArgs(property));
+            }
+        }
+    }
+}
